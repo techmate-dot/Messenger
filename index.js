2ce9b93147f0d7495f67fe455829d528a04e9b99
techmate-dot <snwankwo218@gmail.com>
2023-10-06 17:29:24 +0100

    fisrt commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const axios = require('axios');
+
+const client = new Client({
+        puppeteer: {
+                args: ['--no-sandbox'],
+        },
+        authStrategy: new LocalAuth("/root/wabot")
+})
+
+
+client.on('qr', (qr) => {
+  qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
+});
+
+
+// Mention everyone
+client.on('message', async (msg) => {
+  console.log(`${msg.author} sent ${msg.body}`);
+
+  if(msg.body === '@everyone') {
+      const chat = await msg.getChat();
+
+      let text = "";
+      let mentions = [];
+
+
+      for(let participant of chat.participants) {
+          const contact = await client.getContactById(participant.id._serialized);
+
+          mentions.push(contact);
+          text += `@${participant.id.user} `;
+      }
+        // Assuming you have the 'msg' object representing the received message with a reply
+      if (msg.hasQuotedMsg) {
+        const repliedMessage = await msg.getQuotedMessage();
+        console.log('Bot Replied to message:', repliedMessage.body);
+        await chat.sendMessage(text, { mentions });
+        await repliedMessage.reply("IMPORTANT NOTICE");
+} else {await chat.sendMessage(text, { mentions });}
+
+
+  }
+});
+
+
+client.initialize();
